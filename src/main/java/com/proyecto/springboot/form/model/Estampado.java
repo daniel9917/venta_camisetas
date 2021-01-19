5e4596f9061ae82f202b6ccaf820705a17d77fcf
JDaniel <daniel.jose.45@hotmail.com>
2021-01-19 18:59:16 -0500

    added classes service and controller for estampado & artista

--- src/main/java/com/proyecto/springboot/form/model/Estampado.java
+++ src/main/java/com/proyecto/springboot/form/model/Estampado.java
@@ -1,6 +1,10 @@
 package com.proyecto.springboot.form.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
 
 import javax.persistence.*;
 
@@ -10,25 +14,104 @@ import javax.persistence.*;
 public class Estampado {
 
     @Column(name = "nombre")
+    @Getter
+    @Setter
     private String nombre;
+    
+    
 
-    @Id
+    public Estampado(String nombre, String descripcion, Date fechaRegistro, Long idTema, Long idUsuario) {
+		
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.fechaRegistro = fechaRegistro;
+		this.idTema = idTema;
+		this.idUsuario = idUsuario;
+	}
+
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
+    @Getter
+    @Setter
     private int id;
 
     @Column(name = "descripcion")
+    @Getter
+    @Setter
     private String descripcion;
 
     @Column(name = "fecha_registro", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Getter
+    @Setter
     private java.sql.Date fechaRegistro;
 
     @Column(name = "disponible_venta")
+    @Getter
+    @Setter
     private Boolean disponibleVenta;
 
     @Column(name = "id_tema")
+    @Getter
+    @Setter
     private Long idTema;
 
     @Column(name = "id_usuario")
+    @Getter
+    @Setter
     private Long idUsuario;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public java.sql.Date getFechaRegistro() {
+		return fechaRegistro;
+	}
+
+	public void setFechaRegistro(java.sql.Date fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+
+	public Boolean getDisponibleVenta() {
+		return disponibleVenta;
+	}
+
+	public void setDisponibleVenta(Boolean disponibleVenta) {
+		this.disponibleVenta = disponibleVenta;
+	}
+
+	public Long getIdTema() {
+		return idTema;
+	}
+
+	public void setIdTema(Long idTema) {
+		this.idTema = idTema;
+	}
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+    
+    
 }
---
+++ src/main/java/com/proyecto/springboot/form/services/ArtistaService.java
@@ -0,0 +1,17 @@
+package com.proyecto.springboot.form.services;
+
+import java.util.Collection;
+
+import com.proyecto.springboot.form.model.Estampado;
+
+public class ArtistaService implements IArtistaService{
+	
+	@Override
+	public Collection<Estampado> getEstampados(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}

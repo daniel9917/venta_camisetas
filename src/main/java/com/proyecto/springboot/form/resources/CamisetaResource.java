e86846c730fa061b56d795e518b0eef56740fe12
juancsr <jcsarmientor@correo.udistrital.edu.co>
2021-01-21 00:21:49 -0500

    Camiseta POST Request and Run file for local environment

--- src/main/java/com/proyecto/springboot/form/resources/CamisetaResource.java
+++ src/main/java/com/proyecto/springboot/form/resources/CamisetaResource.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +49,16 @@ public class CamisetaResource {
     })
     public ResponseEntity<Camiseta> findById(@PathVariable("id") int id) {
         return new ResponseEntity<>(this.camisetaService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    @ApiOperation(value = "post", notes = "insert a new record")
+    @ApiResponses(value = {
+        @ApiResponse(code = 202, message = "created")
+        @ApiResponse(code = 404, message = "error")
+    })
+    public ResponseEntity<Camiseta> create() {
+        Camiseta camiseta = new Camiseta();
+        return new ResponseEntity<>(this.camisetaService.create(camiseta), HttpStatus.OK);
     }
 }

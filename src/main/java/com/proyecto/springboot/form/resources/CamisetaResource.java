5a009cab52092164445454291b2888c64d429540
juancsr <jcsarmientor@correo.udistrital.edu.co>
2021-02-03 22:39:34 -0500

    Post endpoint fixed

--- src/main/java/com/proyecto/springboot/form/resources/CamisetaResource.java
+++ src/main/java/com/proyecto/springboot/form/resources/CamisetaResource.java
@@ -1,6 +1,7 @@
 package com.proyecto.springboot.form.resources;
 
 import com.proyecto.springboot.form.model.Camiseta;
+import com.proyecto.springboot.form.resources.DTO.CamisetaDTO;
 import com.proyecto.springboot.form.services.CamisetaService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -8,11 +9,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -53,15 +50,21 @@ public class CamisetaResource {
         return new ResponseEntity<>(this.camisetaService.findById(id), HttpStatus.OK);
     }
 
-    @PostMapping("/create")
+    @PostMapping
     @ApiOperation(value = "post", notes = "insert a new record")
     @ApiResponses(value = {
         @ApiResponse(code = 202, message = "created"),
         @ApiResponse(code = 404, message = "error")
     })
-    public ResponseEntity<Camiseta> create() {
+    public ResponseEntity<Camiseta> create(@RequestBody CamisetaDTO camisetaBody) {
         Camiseta camiseta = new Camiseta();
-        return new ResponseEntity<>(this.camisetaService.create(camiseta), HttpStatus.OK);
+        camiseta.setFechaRegistro(camisetaBody.getFechaRegistro());
+        camiseta.setDescripcion(camisetaBody.getDescripcion());
+        camiseta.setNombre(camisetaBody.getDescripcion());
+        camiseta.setHabilitada(true);
+        camiseta.setFkEstampado(camisetaBody.getFkEstampado());
+        camiseta.setFkTalla(camisetaBody.getFkTalla());
+        return new ResponseEntity<>(this.camisetaService.create(camiseta), HttpStatus.CREATED);
         
     }
 
